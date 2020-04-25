e181043db0bbe9cafd0dad3c69443e7f539eaf3e
susieotto <46828164+susieotto@users.noreply.github.com>
2020-04-25 01:08:59 -0500

    update questions.js

--- questions.js
+++ questions.js
@@ -1,29 +1,34 @@
 // function loadQuestions() {
 const questions = [
     {
-        title: "Primarily, inside which tag of an HTML document do you put the JavaScript?",
-        choices: ["<java>", "<body>", "<script>", "<img>"],
-        answer: "<script>"
+        title: "Question 1: The logical operator that represents 'or' is ____.",
+        choices: ["||", "OR", "&&", "==="],
+        answer: "||"
     },
+
     {
         title: "Question 2: The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
     },
+
     {
-        title: "Which type of pop up box will allow a user to type a response?",
-        choices: ["input", "prompt", "alert", "confirm"],
-        answer: "prompt"
-    },
-    {
-        title: "Question 4: A property of an object that is a function is called a ____.",
+        title: "Question 3: A property of an object that is a function is called a ____.",
         choices: ["method", "string", "stylesheet", "boolean"],
         answer: "method"
     },
+
     {
-        title: "Question 5: The logical operator that represents 'or' is ____.",
-        choices: ["||", "OR", "&&", "==="],
-        answer: "||"
+        title: "Question 4: The instructions for a function are enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "curly brackets"
+    },
+
+    {
+        title: "Question 5: Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+
     }
 ];
 // };
